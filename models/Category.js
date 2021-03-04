574fd26ff7bad6dbf9fcb7fdfaa86540afbc8abd
noplur <geoduckphoto@gmail.com>
2021-03-04 10:03:10 -0800

    routes and columns for Category

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,17 @@ class Category extends Model {}
 Category.init(
   {
     // define columns (refer to user.js in homework???)
+    
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
